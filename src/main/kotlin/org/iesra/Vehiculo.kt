0bc4ef318555d1e582ddaeae60dcabeefb771653
Jesusgallardooo <jgallardo@iesrafaelalberti.es>
2026-01-15 18:22:33 +0100

    ejercicio 2 Vehiculo

--- src/main/kotlin/org/iesra/Vehiculo.kt
+++ src/main/kotlin/org/iesra/Vehiculo.kt
@@ -1,4 +1,28 @@
 package org.iesra
 
-class Vehiculo {
+class Vehiculo (val marca: String, val modelo:String, var kilometraje:Double = 0.0){
+
+    fun registrarViaje(distancia:Double){
+        if (distancia>0){
+            kilometraje += distancia
+        }else{
+            throw IllegalArgumentException("No puedes moverte una distancia nula o negativa.")
+        }
+    }
+
+    override fun toString(): String {
+        return "Vehiculo=(marca=$marca, modelo=$modelo, kilometraje=$kilometraje)"
+    }
+
+}
+
+fun main(){
+    // Crear vehículo
+    var vehiculo: Vehiculo = Vehiculo("Ford", "Fiesta")
+
+    // Realizar viaje
+    vehiculo.registrarViaje(100.0)
+
+    // Mostrar vehículo
+    println(vehiculo)
 }
