7b672d0df5c2a84765ff8a6b3f01ab377e724d95
Jesusgallardooo <jgallardo@iesrafaelalberti.es>
2026-01-15 18:13:23 +0100

    ejercicio 1 Cuenta Bancaria

--- src/main/kotlin/org/iesra/CuentaBancaria.kt
+++ src/main/kotlin/org/iesra/CuentaBancaria.kt
@@ -1,8 +1,38 @@
 package org.iesra
 
 
-class CuentaBancaria (val titular: String, var saldo: Double = 0.0) {
+class CuentaBancaria ( private val titular: String, private var saldo: Double = 0.0) {
+
+    fun ingresar(cantidad:Double): String{
+        if (cantidad >= 0){
+            saldo += cantidad
+        }else {
+            throw IllegalArgumentException("No puedes ingresar una cantidad nula o negativa.")
+        }
+        return "Saldo final: $saldo"
+    }
+
+    fun retirar(cantidad:Double): String{
+        if (cantidad > saldo) {
+            throw IllegalArgumentException("Saldo insuficiente.")
+        }
+        saldo -= cantidad
+        return "Saldo final: $saldo"
+    }
+
+}
 
 
+fun main(){
+
+    // Crear cuenta bancaria con un titular.
+    var cuentaBancaria: CuentaBancaria = CuentaBancaria("Jesús")
+
+    // Realizar un ingreso y un retiro posibles
+    cuentaBancaria.ingresar(100.0)
+    cuentaBancaria.retirar(50.0)
+
+    // Realizar un retiro que no pueda hacerse
+    cuentaBancaria.retirar(60.0)
 
 }
