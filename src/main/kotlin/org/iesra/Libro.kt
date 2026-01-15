232f7bf3c7e944c786d6611b37b6265f5cd8bc7f
Jesusgallardooo <jgallardo@iesrafaelalberti.es>
2026-01-15 19:14:21 +0100

    ejercicio 3 Libro

--- src/main/kotlin/org/iesra/Libro.kt
+++ src/main/kotlin/org/iesra/Libro.kt
@@ -1,4 +1,33 @@
 package org.iesra
 
-class Libro {
+class Libro (val titulo:String, val autor:String, val numPaginas: Int = 100, var leido: Boolean = false) {
+
+    init {
+        require(titulo.isNotBlank()) { "El título no puede estar vacío" }
+        require(autor.isNotBlank()) { "El autor no puede estar vacío" }
+        require(numPaginas in 1..5000) { "El número de páginas debe estar entre 1 y 5000" }
+    }
+
+    constructor(titulo: String, autor: String) : this(
+        titulo,
+        autor,
+        numPaginas = 100,
+        leido = false
+    )
+
+    override fun toString(): String {
+        return "Libro=(titulo=$titulo, autor=$autor, titulo=$titulo, leido=$leido)"
+    }
+
+}
+
+fun main(){
+
+    // Crear 2 libros, uno leído y uno sin leer.
+    var libro1 : Libro = Libro("El mago de Oz", "Paco pepe", 280, false)
+    var libro2 : Libro = Libro("Harry potter", "JuanKa roulin", 340, true)
+
+    // Crear un libro con titulo vacío.
+    var libro3 : Libro = Libro("", "Pepe", 280, false)
+
 }
